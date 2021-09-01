0b551081e86fd728334505a1a118745a6dc75e77
cilioh <cilioh14@gmail.com>
2021-09-01 12:19:44 +0900

    checksum thread creation added

---
+++ jw_make.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -rf *.rpm
+make clean
+
+echo y | yum remove '*spl*'
+
+sh ./autogen.sh
+./configure --with-spec=redhat
+make pkg-utils rpm-dkms
+make rpm-kmod
+
+#echo y | yum localinstall *dkms*.rpm
+#echo y | yum localinstall *.rpm
+
+echo y | yum localinstall *.x86_64.rpm
